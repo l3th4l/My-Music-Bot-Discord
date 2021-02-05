4de8f6d976f57091a350f9cca9fae48fe76ee7d6
itspaulthegreat <arindam.8599@gmail.com>
2021-02-06 02:34:08 +0530

    up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import json
-
+import os
 import discord
 from discord.ext import commands
 
@@ -19,7 +19,8 @@ def main():
     with open('config.json') as fh:
         bot.config = json.load(fh)
 
-    bot.run(bot.config['token'])
+    bot.run(os.environ['token'])
+    
 
 
 if __name__ == "__main__":
